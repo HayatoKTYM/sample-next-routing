8723505188237827a378666f66fd5b40c5a003da
Hayato Katayama（片山颯人） <hayto.katayama@ntt.com>
2023-10-23 20:28:42 +0900

    エラーハンドリングを追加

--- src/pages/nested/[...id]/index.tsx
+++ src/pages/nested/[...id]/index.tsx
@@ -1,14 +1,15 @@
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router';
 
 export default function Page() {
 	// ネストされたパスを受け取るには `...` を追加する
 	// /dynamic/XX -> query.id で XXを取得
 	const router = useRouter();
 	// 分割代入の変数はフォルダ名 = ...idと一致させる
-	const { id } = router.query;
+	const { id, q } = router.query;
 	return (
 		<>
-			<h3>Hello nested {id.join("/")}</h3>
+			{id ? <h3>Hello nested {id.join("/")}</h3> : <h3>query undefined</h3>}
+			{q && <h3> query: {q} </h3>}
 		</>
 	);
 }
